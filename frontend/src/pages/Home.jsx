e173db94c66a8ed0045aa6e569b30ee05ef28fe5
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-05-05 07:02:52 +0500

    Adding Service Section for Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "./../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
 
 const Home = () => {
   return (
@@ -50,6 +51,18 @@ const Home = () => {
             </Col>
 
             <SearchBar />
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
           </Row>
         </Container>
       </section>
