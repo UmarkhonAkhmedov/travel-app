409488eb9e3fc174e2d6751ff9a7d9e8f1a72510
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-05-08 07:29:40 +0500

    Adding Footer Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,7 @@ import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
 import Testimonials from "../components/Testimonial/Testimonials";
+import Newsletter from "../shared/Newsletter";
 
 const Home = () => {
   return (
@@ -154,6 +155,8 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+
+      <Newsletter />
     </>
   );
 };
