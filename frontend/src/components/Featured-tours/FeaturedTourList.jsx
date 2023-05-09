27b0420fb66c09dd0678eef642cda2da7276ff12
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-05-09 09:04:51 +0500

    Creating hooks

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import TourCard from "../../shared/TourCard";
 import tourData from "../../assets/data/tours";
 import { Col } from "reactstrap";
+// import useFetch from "../../hooks/useFetch";
+// import { BASE_URL } from "./../../utils/conflig";
 
 const FeaturedTourList = () => {
+  // const { data: featuredTours, loading, error } = useFetch(
+  //   `${BASE_URL}/search/getFeaturedTours`
+  // );
+
   return (
     <>
+      {/* {laoding && <h4>Loading.........</h4>}
+      {error && <h4>{error}</h4>} */}
       {tourData.map((tour) => (
         <Col lg="3" className="mb-4" key={tour.id}>
           <TourCard tour={tour} />
