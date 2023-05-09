32589be8d301786ae7f66482f5ac5bd06f3d8460
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-05-09 06:30:21 +0500

    Creating tour controller and route

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -65,7 +65,7 @@ export const getSingleTour = async (req, res) => {
   const id = req.params.id;
 
   try {
-    const tour = await Tour.findById(id);
+    const tour = await Tour.findById(id).populate("reviews");
 
     res.status(200).json({
       success: true,
@@ -79,9 +79,60 @@ export const getSingleTour = async (req, res) => {
     });
   }
 };
+
 export const getAllTour = async (req, res) => {
+  const page = parseInt(req.query.page);
+
   try {
-    const tours = await Tour.find({});
+    const tours = await Tour.find({})
+      .populate("reviews")
+      .skip(page * 8)
+      .limit(8);
+
+    res.status(200).json({
+      success: true,
+      count: tours.length,
+      message: "Successful",
+      data: tours,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Not Found",
+    });
+  }
+};
+
+export const getTourBySearch = async (req, res) => {
+  const city = new RegExp(req.query.city, "i");
+  const distance = parseInt(req.query.distance);
+  const maxGroupSize = parseInt(req.query.maxGroupSize);
+
+  try {
+    const tours = await Tour.find({
+      city,
+      distance: { $gte: distance },
+      maxGroupSize: { $gte: maxGroupSize },
+    }).populate("reviews");
+
+    res.status(200).join({
+      success: true,
+      message: "Successful",
+      data: tours,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "Not Found",
+    });
+  }
+};
+
+export const getFeaturedTour = async (req, res) => {
+  try {
+    const tours = await Tour.find({ featured: true })
+      .populate("reviews")
+      .limit(8);
 
     res.status(200).json({
       success: true,
@@ -93,5 +144,15 @@ export const getAllTour = async (req, res) => {
       success: false,
       message: "Not Found",
     });
+  }
+};
+
+export const getTourCount = async (req, res) => {
+  try {
+    const tourCount = await Tour.estimatedDocumentCount();
+
+    res.status(200).json({ success: true, data: tourCount });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to fetch" });
   }
 };
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -5,14 +5,21 @@ import {
   deleteTour,
   updateTour,
   getAllTour,
+  getTourBySearch,
+  getFeaturedTour,
+  getTourCount,
 } from "../controllers/tourController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/", createTour);
-router.put("/:id", updateTour);
-router.delete("/:id", deleteTour);
+router.post("/", verifyAdmin, createTour);
+router.put("/:id", verifyAdmin, updateTour);
+router.delete("/:id", verifyAdmin, deleteTour);
 router.get("/:id", getSingleTour);
-router.get("/", getAllTourr);
+router.get("/", getAllTour);
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTours", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
